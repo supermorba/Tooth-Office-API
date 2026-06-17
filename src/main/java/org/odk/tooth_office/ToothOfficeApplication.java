af6e2eeba064cee69cce6346a1db6e4f18ceada7
Al Banou Sylla <albanou100www@gmail.com>
2026-06-17 15:38:09 +0000

    first commit

---
+++ src/main/java/org/odk/tooth_office/ToothOfficeApplication.java
@@ -0,0 +1,13 @@
+package org.odk.tooth_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToothOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToothOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/odk/tooth_office/ToothOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.odk.tooth_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToothOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
