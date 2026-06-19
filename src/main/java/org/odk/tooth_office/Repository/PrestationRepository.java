dc74b24f9ac86a8d6d669596806b5343035d1714
Aissata <aissata28t@gmail.com>
2026-06-19 14:57:26 +0000

    Prestation controller et service

--- src/main/java/org/odk/tooth_office/Repository/PrestationRepository.java
+++ src/main/java/org/odk/tooth_office/Repository/PrestationRepository.java
@@ -1,4 +1,10 @@
 package org.odk.tooth_office.Repository;
 
-public interface PrestationRepository {
+import org.odk.tooth_office.Entity.Prestation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrestationRepository extends JpaRepository<Prestation,Long> {
+    boolean existsByNomPrestation(String nomPrestation);
 }
--- src/main/java/org/odk/tooth_office/Services/Interfaces/IPrestation.java
+++ src/main/java/org/odk/tooth_office/Services/Interfaces/IPrestation.java
@@ -1,5 +1,18 @@
 package org.odk.tooth_office.Services.Interfaces;
 
-public interface PrestationService {
-    String test();
+import org.odk.tooth_office.DTO.PrestationDTO;
+
+import java.util.List;
+
+public interface IPrestation {
+    PrestationDTO create(PrestationDTO dto);
+
+    PrestationDTO update(Long id, PrestationDTO dto);
+
+    PrestationDTO getById(Long id);
+
+    List<PrestationDTO> getAll();
+
+    void delete(Long id);
+
 }
