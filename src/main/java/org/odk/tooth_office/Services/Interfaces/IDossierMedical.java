7a10a31263498cd52b2d57dfd05b9305cde6f027
maimounasamake <maimounasamake336@gmail.com>
2026-06-22 09:47:56 +0000

    creation repo et services

---
+++ src/main/java/org/odk/tooth_office/Services/Interfaces/IDossierMedical.java
@@ -0,0 +1,20 @@
+package org.odk.tooth_office.Services.Interfaces;
+
+import org.odk.tooth_office.Entity.DossierMedical;
+
+import java.util.List;
+
+public interface IDossierMedical {
+
+    DossierMedical createDossierMedical(DossierMedical dossierMedical);
+
+    DossierMedical getDossierMedicalById(Long id);
+
+    DossierMedical getDossierMedicalByPatientId(Long patientId);
+
+    List<DossierMedical> getAllDossiersMedicaux();
+
+    DossierMedical updateDossierMedical(Long id, DossierMedical dossierMedical);
+
+    void deleteDossierMedical(Long id);
+}
