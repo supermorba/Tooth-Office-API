9b747b4b0169fc004aca9d1779c76a049d24085f
Ismaïla Sangaré <ismailasangare1604@gmail.com>
2026-06-23 11:29:59 +0000

    fixed bugs

--- src/main/java/org/odk/tooth_office/Repository/PlanAbonnementRepository.java
+++ src/main/java/org/odk/tooth_office/Repository/PlanAbonnementRepository.java
@@ -3,16 +3,15 @@ package org.odk.tooth_office.Repository;
 import org.odk.tooth_office.Entity.PlanAbonnement;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PlanAbonnementRepository extends JpaRepository<PlanAbonnement,Long> {
-    List<PlanAbonnement> findByNom(String nom);
 
-    List<PlanAbonnement> findByPrix(Double prix);
 
-    List<PlanAbonnement> findByNomAndPrix(String nom, Double prix);
 
     Optional<PlanAbonnement> findById(Long id); // déjà fourni par JpaRepository
 }
